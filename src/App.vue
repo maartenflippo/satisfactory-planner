b2074cd09fa9f4496413e401039212197618a1b0
Maarten Flippo <maartenflippo@outlook.com>
2023-12-31 00:07:04 +0100

    Create an empty project.
    The project is scaffolded with the Vue template from the vite tool. The
    language is set to TypeScript.
    
    Aside from the scaffold, the PrimeVue component library is added as a
    dependency. Not having to style components will make the tool far easier
    to develop.

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Button from "primevue/button"
+</script>
+
+<template>
+    <h1>Hello World!</h1>
+
+    <Button label="Check Me!" />
+</template>
+
+<style scoped></style>
