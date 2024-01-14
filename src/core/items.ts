aa969a7a2679c7348fd01ee3e343fffb0ce27348
Maarten Flippo <maartenflippo@outlook.com>
2024-01-14 17:44:43 +0100

    Add all smelter and constructor recipes

--- src/core/items.ts
+++ src/core/items.ts
@@ -4,11 +4,41 @@ type CreateItemId<Id extends string> = `item_${Id}`;
  * All the ids for items supported by this application.
  */
 export type ItemId = CreateItemId<"iron_ore">
-    | CreateItemId<"coal_ore">
+    | CreateItemId<"coal">
+    | CreateItemId<"copper_ore">
+    | CreateItemId<"bauxite">
+    | CreateItemId<"caterium_ore">
+    | CreateItemId<"limestone">
+    | CreateItemId<"raw_quartz">
+    | CreateItemId<"sulfur">
+    | CreateItemId<"uranium">
     | CreateItemId<"iron_ingot">
+    | CreateItemId<"copper_ingot">
+    | CreateItemId<"steel_ingot">
+    | CreateItemId<"aluminum_scrap">
+    | CreateItemId<"aluminum_ingot">
+    | CreateItemId<"caterium_ingot">
     | CreateItemId<"iron_plate">
     | CreateItemId<"iron_rod">
     | CreateItemId<"screw">
+    | CreateItemId<"wire">
+    | CreateItemId<"copper_sheet">
+    | CreateItemId<"copper_powder">
+    | CreateItemId<"cable">
+    | CreateItemId<"steel_beam">
+    | CreateItemId<"steel_pipe">
+    | CreateItemId<"empty_canister">
+    | CreateItemId<"empty_fluid_tank">
+    | CreateItemId<"silica">
+    | CreateItemId<"quickwire">
+    | CreateItemId<"quartz_crystal">
+    | CreateItemId<"plastic">
+    | CreateItemId<"rubber">
+    | CreateItemId<"concrete">
+    | CreateItemId<"wood">
+    | CreateItemId<"biomass">
+    | CreateItemId<"aluminum_casing">
+
     | CreateItemId<"reinforced_iron_plate">
     ;
 
@@ -45,10 +75,51 @@ function item(name: string, image_name: string = "", topographic_order: number =
  */
 export const items: Record<ItemId, Item> = {
     item_iron_ore: item("Iron Ore", "iron-ore"),
-    item_coal_ore: item("Coal Ore", "coal"),
+    item_coal: item("Coal Ore", "coal"),
+    item_bauxite: item("Bauxite", "bauxite"),
+    item_copper_ore: item("Copper Ore", "copper-ore"),
+    item_sulfur: item("Sulfur", "sulfur"),
+    item_uranium: item("Uranium", "uranium"),
+    item_limestone: item("Limestone", "limestone"),
+    item_raw_quartz: item("Raw Quartz", "raw-quartz"),
+    item_caterium_ore: item("Caterium Ore", "caterium-ore"),
+    item_wood: item("Wood", "wood"),
+
     item_iron_ingot: item("Iron Ingot", "iron-ingot", 1),
+    item_copper_ingot: item("Copper Ingot", "copper-ingot", 1),
+    item_caterium_ingot: item("Caterium Ingot", "caterium-ingot", 1),
+
+    item_wire: item("Wire", "wire", 2),
+    item_copper_sheet: item("Copper Sheet", "copper-sheet", 2),
+    item_copper_powder: item("Copper Powder", "copper-powder", 2),
+    item_cable: item("Cable", "cable", 3),
+
     item_iron_plate: item("Iron Plate", "iron-plate", 2),
     item_iron_rod: item("Iron Rod", "iron-rod", 2),
     item_screw: item("Screw", "screw", 3),
+
+    item_silica: item("Silica", "silica", 1),
+    item_quartz_crystal: item("Quartz Crystal", "quartz-crystal", 1),
+
+    item_biomass: item("Biomass", "biomass", 1),
+
+    item_plastic: item("Plastic", "plastic", 1),
+    item_rubber: item("Rubber", "rubber", 1),
+
+    item_concrete: item("Concrete", "concrete", 1),
+
+    item_quickwire: item("Quickwire", "quickwire", 2),
+
+    item_steel_ingot: item("Steel Ingot", "steel-ingot", 1),
+    item_steel_beam: item("Steel Beam", "steel-beam", 2),
+    item_steel_pipe: item("Steel Pipe", "steel-pipe", 2),
+
+    item_empty_canister: item("Empty Canister", "empty-canister", 2),
+    item_empty_fluid_tank: item("Empty Fluid Tank", "empty-fluid-tank", 2),
+
     item_reinforced_iron_plate: item("Reinforced Iron Plate", "reinforced-iron-plate", 4),
+
+    item_aluminum_scrap: item("Aluminum Scrap", "aluminum-scrap", 4),
+    item_aluminum_ingot: item("Aluminum Ingot", "aluminum-ingot", 5),
+    item_aluminum_casing: item("Aluminum Casing", "aluminum-casing", 6),
 };
--- src/core/recipes.ts
+++ src/core/recipes.ts
@@ -33,6 +33,11 @@ export type Recipe = {
     name: string,
 
     /**
+     * Whether this is an alternate recipe.
+     */
+    alternate: boolean,
+
+    /**
      * The machine this recipe belongs to.
      */
     get machine(): Machine;
@@ -70,7 +75,24 @@ export type Recipe = {
 //     };
 // }
 
-function ConstructorRecipe(name: string, input: ItemRecipeComponent, output: ItemRecipeComponent): Recipe {
+function SmelterRecipe(name: string, input: ItemRecipeComponent, output: ItemRecipeComponent): Recipe {
+    const power_input: PowerRecipeComponent = {
+        type: "power",
+        amount: machines["machine_smelter"].base_power_consumption,
+    };
+
+    return {
+        name,
+        inputs: [power_input, input],
+        outputs: [output],
+        alternate: false,
+        get machine() {
+            return machines["machine_smelter"];
+        }
+    };
+}
+
+function ConstructorRecipe(name: string, input: ItemRecipeComponent, output: ItemRecipeComponent, alternate: boolean = false): Recipe {
     const power_input: PowerRecipeComponent = {
         type: "power",
         amount: 4,
@@ -78,6 +100,7 @@ function ConstructorRecipe(name: string, input: ItemRecipeComponent, output: Ite
 
     return {
         name,
+        alternate,
         inputs: [power_input, input],
         outputs: [output],
         get machine() {
@@ -86,23 +109,8 @@ function ConstructorRecipe(name: string, input: ItemRecipeComponent, output: Ite
     };
 }
 
-function SmelterRecipe(name: string, input: ItemRecipeComponent, output: ItemRecipeComponent): Recipe {
-    const power_input: PowerRecipeComponent = {
-        type: "power",
-        amount: 4,
-    };
 
-    return {
-        name,
-        inputs: [power_input, input],
-        outputs: [output],
-        get machine() {
-            return machines["machine_smelter"];
-        }
-    };
-}
-
-function AssemblerRecipe(name: string, inputs: [ItemRecipeComponent, ItemRecipeComponent], output: ItemRecipeComponent): Recipe {
+function AssemblerRecipe(name: string, inputs: [ItemRecipeComponent, ItemRecipeComponent], output: ItemRecipeComponent, alternate: boolean = false): Recipe {
     const power_input: PowerRecipeComponent = {
         type: "power",
         amount: 15,
@@ -110,6 +118,7 @@ function AssemblerRecipe(name: string, inputs: [ItemRecipeComponent, ItemRecipeC
 
     return {
         name,
+        alternate,
         inputs: [power_input, ...inputs],
         outputs: [output],
         get machine() {
@@ -125,9 +134,33 @@ export type RecipeId =
     | CreateRecipeId<"coal_ore">
     |*/
     CreateRecipeId<"iron_ingot">
+    | CreateRecipeId<"copper_ingot">
+    | CreateRecipeId<"caterium_ingot">
+    | CreateRecipeId<"aluminum_ingot">
     | CreateRecipeId<"iron_plate">
     | CreateRecipeId<"iron_rod">
     | CreateRecipeId<"screw">
+    | CreateRecipeId<"aluminum_casing">
+    | CreateRecipeId<"biocoal">
+    | CreateRecipeId<"cable">
+    | CreateRecipeId<"cast_screw">
+    | CreateRecipeId<"caterium_wire">
+    | CreateRecipeId<"charcoal">
+    | CreateRecipeId<"concrete">
+    | CreateRecipeId<"copper_powder">
+    | CreateRecipeId<"copper_sheet">
+    | CreateRecipeId<"empty_canister">
+    | CreateRecipeId<"empty_fluid_tank">
+    | CreateRecipeId<"iron_wire">
+    | CreateRecipeId<"quartz_crystal">
+    | CreateRecipeId<"quickwire">
+    | CreateRecipeId<"silica">
+    | CreateRecipeId<"steel_beam">
+    | CreateRecipeId<"steel_canister">
+    | CreateRecipeId<"steel_pipe">
+    | CreateRecipeId<"steel_rod">
+    | CreateRecipeId<"steel_screw">
+    | CreateRecipeId<"wire">
     | CreateRecipeId<"reinforced_iron_plate">
     ;
 
@@ -148,6 +181,24 @@ export const recipes: Record<RecipeId, Recipe> = {
         ItemComponent("item_iron_ingot", 30),
     ),
 
+    recipe_copper_ingot: SmelterRecipe(
+        "Copper Ingot",
+        ItemComponent("item_copper_ore", 30),
+        ItemComponent("item_copper_ingot", 30),
+    ),
+
+    recipe_aluminum_ingot: SmelterRecipe(
+        "Aluminum Ingot",
+        ItemComponent("item_aluminum_scrap", 60),
+        ItemComponent("item_aluminum_ingot", 30),
+    ),
+
+    recipe_caterium_ingot: SmelterRecipe(
+        "Caterium Ingot",
+        ItemComponent("item_caterium_ore", 45),
+        ItemComponent("item_caterium_ingot", 15),
+    ),
+
     recipe_iron_plate: ConstructorRecipe(
         "Iron Plates",
         ItemComponent("item_iron_ingot", 30),
@@ -166,10 +217,145 @@ export const recipes: Record<RecipeId, Recipe> = {
         ItemComponent("item_screw", 40),
     ),
 
+    recipe_wire: ConstructorRecipe(
+        "Wire",
+        ItemComponent("item_copper_ingot", 15),
+        ItemComponent("item_wire", 30),
+    ),
+
+    recipe_steel_screw: ConstructorRecipe(
+        "Steel Screw",
+        ItemComponent("item_steel_beam", 5),
+        ItemComponent("item_screw", 52),
+        true,
+    ),
+
+    recipe_steel_rod: ConstructorRecipe(
+        "Steel Rod",
+        ItemComponent("item_steel_ingot", 5),
+        ItemComponent("item_iron_rod", 48),
+        true,
+    ),
+
+    recipe_steel_pipe: ConstructorRecipe(
+        "Steel Pipe",
+        ItemComponent("item_steel_ingot", 12),
+        ItemComponent("item_steel_pipe", 20),
+    ),
+
+    recipe_steel_canister: ConstructorRecipe(
+        "Steel Canister",
+        ItemComponent("item_steel_ingot", 60),
+        ItemComponent("item_empty_canister", 40),
+        true,
+    ),
+
+    recipe_steel_beam: ConstructorRecipe(
+        "Steel Beam",
+        ItemComponent("item_steel_ingot", 60),
+        ItemComponent("item_steel_beam", 15),
+    ),
+
+    recipe_silica: ConstructorRecipe(
+        "Silica",
+        ItemComponent("item_raw_quartz", 22.5),
+        ItemComponent("item_silica", 37.5),
+    ),
+
+    recipe_quickwire: ConstructorRecipe(
+        "Quickwire",
+        ItemComponent("item_caterium_ingot", 12),
+        ItemComponent("item_quickwire", 60),
+    ),
+
+    recipe_quartz_crystal: ConstructorRecipe(
+        "Quartz Crystal",
+        ItemComponent("item_raw_quartz", 37.5),
+        ItemComponent("item_quartz_crystal", 22.5),
+    ),
+
+    recipe_iron_wire: ConstructorRecipe(
+        "Iron Wire",
+        ItemComponent("item_iron_ingot", 12.5),
+        ItemComponent("item_wire", 22.5),
+        true,
+    ),
+
+    recipe_empty_fluid_tank: ConstructorRecipe(
+        "Empty Fluid Tank",
+        ItemComponent("item_aluminum_ingot", 60),
+        ItemComponent("item_empty_fluid_tank", 60),
+    ),
+
+    recipe_empty_canister: ConstructorRecipe(
+        "Empty Canister",
+        ItemComponent("item_plastic", 30),
+        ItemComponent("item_empty_canister", 60),
+    ),
+
+    recipe_copper_sheet: ConstructorRecipe(
+        "Copper Sheet",
+        ItemComponent("item_copper_ingot", 20),
+        ItemComponent("item_copper_sheet", 10),
+    ),
+
+    recipe_copper_powder: ConstructorRecipe(
+        "Copper Powder",
+        ItemComponent("item_copper_ingot", 300),
+        ItemComponent("item_copper_powder", 50),
+    ),
+
+    recipe_concrete: ConstructorRecipe(
+        "Concrete",
+        ItemComponent("item_limestone", 45),
+        ItemComponent("item_concrete", 15),
+    ),
+
+    recipe_charcoal: ConstructorRecipe(
+        "Charcoal",
+        ItemComponent("item_wood", 15),
+        ItemComponent("item_coal", 150),
+        true,
+    ),
+
+    recipe_caterium_wire: ConstructorRecipe(
+        "Caterium Wire",
+        ItemComponent("item_caterium_ingot", 15),
+        ItemComponent("item_wire", 120),
+        true,
+    ),
+
+    recipe_cast_screw: ConstructorRecipe(
+        "Cast Screw",
+        ItemComponent("item_iron_ingot", 12.5),
+        ItemComponent("item_screw", 50),
+        true,
+    ),
+
+    recipe_cable: ConstructorRecipe(
+        "Cable",
+        ItemComponent("item_wire", 60),
+        ItemComponent("item_cable", 30),
+    ),
+
+    recipe_biocoal: ConstructorRecipe(
+        "Biocoal",
+        ItemComponent("item_biomass", 37.5),
+        ItemComponent("item_coal", 45),
+        true,
+    ),
+
+    recipe_aluminum_casing: ConstructorRecipe(
+        "Aluminum Casing",
+        ItemComponent("item_aluminum_ingot", 90),
+        ItemComponent("item_aluminum_casing", 60),
+    ),
+
     recipe_reinforced_iron_plate: AssemblerRecipe(
         "Reinforced Iron Plates",
         [ItemComponent("item_iron_plate", 30), ItemComponent("item_screw", 60)],
         ItemComponent("item_reinforced_iron_plate", 5),
     ),
+
 };
 
